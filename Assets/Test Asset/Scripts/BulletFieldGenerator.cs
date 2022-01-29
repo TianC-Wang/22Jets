990a2914c16abd0bcd9f581bc0ec40bd7019d683
TianC Wang <thewater2016@qq.com>
2022-01-30 00:26:03 +0800

    Small changes3

--- Assets/Test Asset/Scripts/BulletFieldGenerator.cs
+++ Assets/Test Asset/Scripts/BulletFieldGenerator.cs
@@ -29,7 +29,7 @@ namespace Tests.Components
                                 bullet.transform.position = _min + new Vector3(0f, Random.Range(0f, .5f) * (_max.y - _min.y), Random.Range(0f, 1f) * (_max.z - _min.z));
                             else
                                 bullet.transform.position = _max - new Vector3(0f, Random.Range(.5f, 1f) * (_max.y - _min.y), Random.Range(0f, 1f) * (_max.z - _min.z));
-                            bullet.GetComponent<Rigidbody>().velocity = ((_max + _min) / 2f - bullet.transform.position).normalized * 10f;
+                            bullet.GetComponent<Rigidbody>().velocity = ((_max + _min) / 2f - bullet.transform.position).normalized * 25f;
                             bullet.AddComponent<SimpleBullet>().SetOutRange(_min - Vector3.one * 255f, _max + Vector3.one * 255f);
                         }
                         break;
